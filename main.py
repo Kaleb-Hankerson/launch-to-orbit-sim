1b3c0ac7161d5451853d3e71fc5447edece53ef7
Kaleb Hankerson <kchank02@gmail.com>
2026-08-24 14:05:20 -0500

    2D ascent is working correctly. Launch angle, pitch over is working but linear, trajectory and velocity plots confirm expected 2D dynamics.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from rocket import Rocket
 import matplotlib.pyplot as plt
+import numpy as np
 
 print("Welcome to the Launch-to-Orbit Simulator!\n")
 mass = float(input("What is the total mass of your rocket?\n"))
@@ -12,8 +13,12 @@ mass_flow_rate = float(input("What is the mass flow rate of your rocket?\n"))
 dt = 0.1
 time = 0
 time_list = []
-height_list = []
-velocity_list = []
+x_list = []
+y_list = []
+vx_list = []
+vy_list = []
+speed_list = []
+
 
 my_rocket = Rocket(mass,dry_mass,exhaust_velocity,mass_flow_rate)
 
@@ -21,23 +26,35 @@ my_rocket = Rocket(mass,dry_mass,exhaust_velocity,mass_flow_rate)
 #The time and heights at each step are stored in respective lists
 while True:
     my_rocket.update()
+    vx_list.append(my_rocket.velocity[0])
+    vy_list.append(my_rocket.velocity[1])
+    speed_list.append(np.linalg.norm(my_rocket.velocity))
     time += dt
     time_list.append(time)
-    height_list.append(my_rocket.height)
-    velocity_list.append(my_rocket.velocity)
-    if my_rocket.height <=0:
+    x_list.append(my_rocket.position[0])
+    y_list.append(my_rocket.position[1])
+    if my_rocket.position [1] <= 0:
         break
 
-#Height over time plot
-plt.plot(time_list, height_list)
-plt.xlabel("Time (s)")
-plt.ylabel("Height (m)")
-plt.title("Height over time plot")
+#X over Y plot
+plt.plot(x_list, y_list)
+plt.xlabel("Horizontal movement (m)")
+plt.ylabel("Vertical movement (m)")
+plt.title("X over Y plot")
 plt.show()
 
-#Velocity over time plot
-plt.plot(time_list, velocity_list)
+#Velocity components over time plot
+plt.plot(time_list, vx_list, label="Horizontal velocity (vx)")
+plt.plot(time_list, vy_list, label="Vertical velocity (vy)")
 plt.xlabel("Time (s)")
 plt.ylabel("Velocity (m/s)")
-plt.title("Velocity over time plot")
+plt.title("Velocity Components Over Time")
+plt.legend()
+plt.show()
+
+#Speed over time plot
+plt.plot(time_list, speed_list)
+plt.xlabel("Time (s)")
+plt.ylabel("Speed (m/s)")
+plt.title("Total Speed Over Time")
 plt.show()
